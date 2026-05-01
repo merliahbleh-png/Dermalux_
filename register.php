6641004de1cfbf2a82da89a9e68fdc85760d25af
mehak <merliahbleh@gmail.com>
2026-05-02 01:59:49 +0200

    Update register.php

--- register.php
+++ register.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Registrazione - Dermalux</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
